51796466096daf4d5c8160e7c7b15c6ae48a3f70
Luiz Navarro <79621478+luizndh@users.noreply.github.com>
2023-01-31 19:56:39 -0300

    Update multigraph.py

--- códigos/multigraph.py
+++ códigos/multigraph.py
@@ -1,8 +1,6 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import pandas as pd
-from networkx.algorithms import community
-from networkx.algorithms import centrality
 
 # Removendo todas as linhas que possuem "NA" como BR
 with open('dados_datatran2020.csv', 'r') as input_file, open('dados_sem_na.csv', 'w') as output_file:
@@ -58,34 +56,6 @@ for i in range(len(bd.index)):
     linha_arq = bd.iloc[i]
     G.add_edge(linha_arq['br'], linha_arq['municipio'], data=linha_arq['data_inversa'], acidente=linha_arq['tipo_acidente'])
     
-# Peso total do grafo, ou seja, quantidade de acidentes no total
-print(G.size(weight='weight')) # 7394 acidentes
-
-"""
-- Menor grau - Município Nova Ponte: grau 0.00505, Maior grau - BR 381, grau 11.80303
-- Os nós com as BR's são os que possuem um maior grau. Isso acontece pois são os nós com o maior número de interações, ou seja,
-são os nós mais "importantes" do grafo. Alguns municípios estão com maior grau do que outras BR's, como por exemplo 
-Betim, com grau 2.6161, Uberlandia, com grau 1.9898, Uberaba, com grau 1.6717, dentre outros. Isso pode ser explicado por serem municípios
-populares e consequentemente mais visitados, o que aumenta a chance de acontecer acidentes nas BR's ligadas a estes municípios.
-Além disso, esse mesmo resultado pode ser obtido ao verificar o grau de cada nó, já que um nó com o maior grau
-também vai ser o nó com maior grau de centralidade.
-
-Descobrindo e ordenando o grau de centralidade dos nós do grafo:
- """
-dicionario = centrality.degree_centrality(G)
-sorted_dicionario = sorted(dicionario.items(), key=lambda kv: kv[1])
-print(sorted_dicionario) 
-# [('NOVA PONTE', 0.005050505050505051), ... (381, 11.803030303030305)]
-
-
-print(community.louvain_communities(G))
-
-# Mostra a quantidade de acidentes em cada nó
-print(nx.degree(G, weight='weight'))
-# [('PERDOES', 61), ('UBERABA', 331), ('RIO MANSO', 10), (40, 1600), etc...
-
-# Ou em um nó específico
-print(nx.degree(G, 'UBERABA')) # 331 acidentes
 
 # É possível verificar o tipo de acidente, a data e em qual BR/Município foi o acidente utilizando o comando abaixo.
 print(G[265])
@@ -109,11 +79,12 @@ print(G['PLANURA'])
 # 12: {'data': '11/28/2020', 'acidente': 'Colisão lateral'}, 
 # 13: {'data': '11/28/2020', 'acidente': 'Saída de leito carroçável'}}}
 
+
 # Plotando o grafo
 plt.figure(3,figsize=(8,8), dpi=250)
 
 # O método Kamada Kawai abaixo plota o grafo de uma forma que as arestas dele 
 # tendem a não cortar outras arestas do grafo, ou seja, os nós que estão juntos
 # costumam estar próximos geograficamente.
-nx.draw_kamada_kawai(G, node_color=cores_tipo, node_size=120, font_size=8)
+nx.draw_kamada_kawai(G, with_labels=True, node_color=cores_tipo, node_size=120, font_size=8)
 plt.show()
